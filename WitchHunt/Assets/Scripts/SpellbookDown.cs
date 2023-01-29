d0abf2a8f3d8a0db391193b27bae585d6cafdc74
NAmin23 <NAmin23@uchicago.edu>
2023-01-29 01:50:18 -0500

    Book import and controller script

--- WitchHunt/Assets/Scripts/SpellbookDown.cs
+++ WitchHunt/Assets/Scripts/SpellbookDown.cs
@@ -1,14 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using System;
 
 public class SpellbookDown : MonoBehaviour
 {
-  private void OnTriggerEnter(Collider other)
-  {
-    if (other.gameObject.name == "OVRHandPrefabL")
+    public TMP_Text leftInputText;
+    public GameObject spellbook;
+    public Transform spellbookOrigin;
+    public Transform spellbookDest;
+    private float j = 0; // iterator for linear interpolation
+    public GameObject spellbookUp;
+    private bool lowering = false;
+
+    private void OnTriggerEnter(Collider other)
     {
-      Debug.Log("goodbye");
+        if (other.gameObject.name == "OVRHandPrefabL")
+        {
+            String temp = "Dismiss";
+            leftInputText.text = temp;
+
+            j = 0;
+            lowering = true;
+            spellbook.transform.position = spellbookDest.position;
+        }
     }
-  }
+
+    void Update()
+    {
+        if ((spellbook.activeSelf == true) && j < 1 && (spellbookUp.GetComponent<SpellbookUp>().raising == false) && (lowering == true))
+        {
+            spellbook.transform.position = Vector3.Lerp(spellbookDest.position, spellbookOrigin.position, j);
+            j = j + 0.02f;
+            leftInputText.text = j.ToString();
+        }
+
+        if (j >= 1 && (Vector3.Distance(spellbook.transform.position, spellbookOrigin.position) < .02f))
+        {
+            spellbook.SetActive(false);
+            lowering = false;
+        }
+    }
 }
--- WitchHunt/Assets/Scripts/SpellbookUp.cs
+++ WitchHunt/Assets/Scripts/SpellbookUp.cs
@@ -1,14 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using System;
 
 public class SpellbookUp : MonoBehaviour
 {
+    public TMP_Text leftInputText;
+    public GameObject spellbook;
+    public Transform spellbookOrigin;
+    public Transform spellbookDest;
+    private float i = 0; // iterator for linear interpolation
+    public bool raising = false;
+
     private void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.name == "OVRHandPrefabL")
+        if (other.gameObject.name == "OVRHandPrefabL")
         {
-            Debug.Log("hello");
+            String temp = "Raise";
+            leftInputText.text = temp;
+
+            spellbook.SetActive(true);
+            spellbook.transform.position = spellbookOrigin.position;
+            i = 0;
+            raising = true;
+        }
+    }
+
+    void Update()
+    {
+        if ((spellbook.activeSelf == true) && (raising == true)) ;
+        {
+            if (i < 1)
+            {
+                spellbook.transform.position = Vector3.Lerp(spellbookOrigin.position, spellbookDest.position, i);
+                i = i + 0.02f;
+            }
+        }
+
+        if(i >= 1)
+        {
+            raising = false;
         }
     }
 }
