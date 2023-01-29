fde18fa13fdab1ff77658fba6b48f43a42c865f0
tt5 <tallonkh@gmail.com>
2023-01-29 15:11:52 -0600

    simple cursor camera controls

--- WitchHunt/Assets/Scripts/PancakeController.cs
+++ WitchHunt/Assets/Scripts/PancakeController.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class PancakeController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject targetObject;
+    public float Sensitivity
     {
-        
+        get { return sensitivity; }
+        set { sensitivity = value; }
     }
+    [Range(0.1f, 9f)][SerializeField] float sensitivity = 2f;
+    [Tooltip("Limits vertical camera rotation. Prevents the flipping that happens when rotation goes above 90.")]
+    [Range(0f, 90f)][SerializeField] float yRotationLimit = 88f;
 
-    // Update is called once per frame
+    Vector2 rotation = Vector2.zero;
+    const string xAxis = "Mouse X"; //Strings in direct code generate garbage, storing and re-using them creates no garbage
+    const string yAxis = "Mouse Y";
+
     void Update()
     {
-        
+        rotation.x += Input.GetAxis(xAxis) * sensitivity;
+        rotation.y += Input.GetAxis(yAxis) * sensitivity;
+        rotation.y = Mathf.Clamp(rotation.y, -yRotationLimit, yRotationLimit);
+        var xQuat = Quaternion.AngleAxis(rotation.x, Vector3.up);
+        var yQuat = Quaternion.AngleAxis(rotation.y, Vector3.left);
+
+        targetObject.transform.localRotation = xQuat * yQuat;
     }
 }
