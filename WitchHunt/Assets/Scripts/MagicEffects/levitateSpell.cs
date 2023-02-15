8350baf32fc9c08d81d46aa231c59270901af1dd
NAmin23 <NAmin23@uchicago.edu>
2023-02-15 18:26:26 -0500

    messing around honestly

--- WitchHunt/Assets/Scripts/MagicEffects/levitateSpell.cs
+++ WitchHunt/Assets/Scripts/MagicEffects/levitateSpell.cs
@@ -17,23 +17,36 @@ public class levitateSpell : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.C))
+        if (Input.GetKeyDown(KeyCode.W))
         {
-            //potions = GameObject.FindGameObjectsWithTag("Potion");
-            potions = GameObject.Find("Potion");
+            potions = GameObject.FindGameObjectsWithTag("Potion");
+            //potions = GameObject.Find("Potion");
 
             if (potions != null)
             {
                 Debug.Log("ITS NULL");
             }
 
-            for (i = 0; i < potions.Length; i++)
+            for (i = 0; i < 1; i++)
             {
-                potion = potions[i];
-                potion.transform.position = potion.transform.position + new Vector3(0.0f, 10.0f, 0.0f);
-
+                potion = GameObject.Find("Potion3(Clone)");
+                potion.transform.position += new Vector3(0.0f, Random.Range(1.0f, 5.0f), 0.0f);
             }
         }
-        
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            GameObject.Find("Potion2(Clone)").transform.position += new Vector3(0.0f, Random.Range(1.0f, 5.0f), 0.0f);
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            GameObject.Find("Potion4(Clone)").transform.position += new Vector3(0.0f, Random.Range(1.0f, 5.0f), 0.0f);
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            GameObject.Find("Potion5(Clone)").transform.position += new Vector3(0.0f, Random.Range(1.0f, 5.0f), 0.0f);
+        }
+
     }
 }
