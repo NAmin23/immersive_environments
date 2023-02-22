ffd695ee5ef0419fd259631d84ed535289dffe65
tt5 <tallonkh@gmail.com>
2023-02-22 17:54:43 -0600

    vegan potions

--- WitchHunt/Assets/Scripts/RandomColor.cs
+++ WitchHunt/Assets/Scripts/RandomColor.cs
@@ -8,9 +8,11 @@ public class RandomColor : MonoBehaviour
     void Start()
     {
         var mr = this.GetComponent<MeshRenderer>();
+
         mr.material.SetColor("_Base_Color", new Color(Random.value, Random.value, Random.value));
-        mr.material.SetColor("_Emission", new Color(Random.value, Random.value, Random.value));
-        mr.material.SetColor("_Emission_2", new Color(Random.value, Random.value, Random.value));
+        mr.material.SetColor("_Base_Color_2", new Color(Random.value, Random.value, Random.value));
+        mr.material.SetColor("_Emission", Color.HSVToRGB(Random.value, 1.0f, 1.0f) * Random.value * 10.0f);
+        mr.material.SetColor("_Emission_2", Color.HSVToRGB(Random.value, 1.0f, 1.0f));
     }
 
     // Update is called once per frame
