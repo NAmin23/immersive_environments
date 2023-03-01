72700119f1ee44ef8f346e747d3437d6c9412391
NAmin23 <NAmin23@uchicago.edu>
2023-02-28 21:34:01 -0500

    bubble and shrink spell

--- WitchHunt/Assets/Scripts/MagicEffects/MagicController.cs
+++ WitchHunt/Assets/Scripts/MagicEffects/MagicController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,11 +8,31 @@ public class MagicController : MonoBehaviour
     public GameObject pancake;
     public Transform pancakeTransform;
 
+    public GameObject particleEffect;
+
+    public List<GameObject> shrunkObjects;
+
     public void spawnPancake()
     {
         GameObject newPancake;
         newPancake = Instantiate(pancake, pancakeTransform);
         newPancake.SetActive(true);
-        Debug.Log("pancake spawned");
+
+        particleEffect.SetActive(false);
+    }
+
+    public void spawnBubbles()
+    {
+        particleEffect.SetActive(true);
+    }
+
+    public void shrinkObjects()
+    {
+        foreach (var shrinkObject in shrunkObjects)
+        {
+            shrinkObject.transform.localScale *= UnityEngine.Random.Range(0.1f, 3.0f);
+        }
+
+        particleEffect.SetActive(false);
     }
 }
